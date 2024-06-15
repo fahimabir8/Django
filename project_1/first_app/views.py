a7cbcc7ea9c15b83b9868f9331f98008bc76b8c8
fahimabir8 <iamabir444@gmail.com>
2024-06-15 12:44:40 +0600

    Module 1 codes

---
+++ project_1/first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def courses(request):
+    return HttpResponse("This is first app/ Courses page.")
+
+def about(request):
+    return HttpResponse("This is first app/ About page.")
+
+def home(request):
+    return HttpResponse("This is first app page.")
