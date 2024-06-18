9d1555f03919726ba2c92c14e6aa0940c02412d7
fahimabir8 <iamabir444@gmail.com>
2024-06-18 12:15:07 +0600

    Templates

--- project_3/first_app/views.py
+++ project_3/first_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    d = {'author': 'jelly', 'age':29, 'list' : [2,3,4]}
+    d = {'author': 'jelly', 'age':9, 'list' : [2,3,4]}
     return render(request,'home.html',d)
