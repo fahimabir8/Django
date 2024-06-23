51b5cff9a06a5e7956ed9b12cfaf05054ed68de2
fahimabir8 <iamabir444@gmail.com>
2024-06-23 12:47:40 +0600

    profile forms ready

--- blog_project/author/views.py
+++ blog_project/author/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from . import forms
-
 # Create your views here.
+
 def add_author(request):
     if request.method == 'POST':
-        author_form = forms.AuthorForm()
+        author_form = forms.AuthorForm(request.POST)
         if author_form.is_valid():
             author_form.save()
             return redirect('add_author')
-        else:
-            author_form = forms.AuthorForm()    
-    return render(request, 'add_author.html', {'form': author_form})
+    
+    else:
+        author_form = forms.AuthorForm()
+    return render(request, 'add_author.html', {'form' : author_form})
