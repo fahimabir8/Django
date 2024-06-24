ba27275bf5f3db041a86739c4ecae72d4ae61240
fahimabir8 <iamabir444@gmail.com>
2024-06-24 23:13:02 +0600

    blog project created

--- blog_project/blog_project part 1/blog_project/urls.py
+++ blog_project/blog_project part 1/blog_project/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', views.home, name="homepage"),
     path('author/', include('author.urls') ),
     path('profile/', include('profiles.urls') ),
     path('post/', include('posts.urls') ),
