998b3112dcbb1c667e6bb739c04fbddcd6e3a71b
fahimabir8 <iamabir444@gmail.com>
2024-06-28 05:58:35 +0600

    register page done

--- blog_project part 2/blog_project/author/views.py
+++ blog_project part 2/blog_project/author/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render, redirect
 from . import forms
 # Create your views here.
 
-def add_author(request):
+# def add_author(request):
+#     if request.method == 'POST':
+#         author_form = forms.AuthorForm(request.POST)
+#         if author_form.is_valid():
+#             author_form.save()
+#             return redirect('add_author')
+    
+#     else:
+#         author_form = forms.AuthorForm()
+#     return render(request, 'add_author.html', {'form' : author_form})
+
+def register(request):
     if request.method == 'POST':
-        author_form = forms.AuthorForm(request.POST)
-        if author_form.is_valid():
-            author_form.save()
-            return redirect('add_author')
+        register_form = forms.RegistrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            return redirect('register')
     
     else:
-        author_form = forms.AuthorForm()
-    return render(request, 'add_author.html', {'form' : author_form})
+        register_form = forms.RegistrationForm()
+    return render(request, 'register.html', {'form' : register_form})
+
