03a44524a111daac9242275a333bd8f5d6a67b7b
fahimabir8 <iamabir444@gmail.com>
2024-07-05 15:10:36 +0600

    update delete in class

--- blog_project part 3/blog_project/posts/views.py
+++ blog_project part 3/blog_project/posts/views.py
@@ -2,37 +2,64 @@ from django.shortcuts import render,redirect
 from . import forms 
 from . import models
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
-@login_required
-def add_post(request):
-    if (request.method == 'POST'):
-        post_form = forms.PostForm(request.POST)
-        if post_form.is_valid():
-            # post_form.cleaned_data['author'] = request.user
-            post_form.instance.author = request.user
-            post_form.save()
-            return redirect('add_post')
+# @login_required
+# def add_post(request):
+#     if (request.method == 'POST'):
+#         post_form = forms.PostForm(request.POST)
+#         if post_form.is_valid():
+#             # post_form.cleaned_data['author'] = request.user
+#             post_form.instance.author = request.user
+#             post_form.save()
+#             return redirect('add_post')
         
-    else:
-        post_form = forms.PostForm()
-    return render(request, 'add_post.html' , {'form': post_form})
+#     else:
+#         post_form = forms.PostForm()
+#     return render(request, 'add_post.html' , {'form': post_form})
 
-@login_required
-def edit_post(request,id):
-    post = models.Post.objects.get(pk=id)
-    post_form = forms.PostForm(instance=post)
-    if (request.method == 'POST'):
-        post_form = forms.PostForm(request.POST,instance=post)
-        if post_form.is_valid():
-            post_form.instance.author = request.user
-            post_form.save()
-            return redirect('profile')
+#Add Post Using class based view
+class AddPostCreateView(CreateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    success_url = reverse_lazy('add_post')
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# @login_required
+# def edit_post(request,id):
+#     post = models.Post.objects.get(pk=id)
+#     post_form = forms.PostForm(instance=post)
+#     if (request.method == 'POST'):
+#         post_form = forms.PostForm(request.POST,instance=post)
+#         if post_form.is_valid():
+#             post_form.instance.author = request.user
+#             post_form.save()
+#             return redirect('profile')
         
-    return render(request, 'add_post.html' , {'form': post_form})
+#     return render(request, 'add_post.html' , {'form': post_form})
 
-@login_required
-def delete_post(request, id):
-    post = models.Post.objects.get(pk=id)
-    post.delete()
-    return redirect('homepage')
+class EditPostView(UpdateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('profile')
+
+
+# @login_required
+# def delete_post(request, id):
+#     post = models.Post.objects.get(pk=id)
+#     post.delete()
+#     return redirect('homepage')
+
+class DeletePostView(DeleteView):
+    model = models.Post
+    template_name = 'delete.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('profile')
